aa28b522e1371c58d7b48c8bb2d2dd3fdb99136b
Milos Kurjacki <10221miloskurjacki@etsntesla.edu.rs>
2024-04-23 15:36:07 +0200

    drugi deo

--- src/main/java/org/etsntesla/it/controllers/EmocijeController.java
+++ src/main/java/org/etsntesla/it/controllers/EmocijeController.java
@@ -1,11 +1,42 @@
 package org.etsntesla.it.controllers;
 
+import org.etsntesla.it.models.Emocije;
+import org.etsntesla.it.repositories.EmocijeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmocijeController {
 
     @Autowired
-    EmocijeService service;
+    EmocijeRepository emocijeRepository;
+
+    @RequestMapping(path = "/emocije", method = RequestMethod.GET)
+    public List<Emocije> getAll(){
+        return emocijeRepository.findAll();
+    }
+
+    @RequestMapping(path = "/emocije/{id}", method = RequestMethod.GET)
+    public Emocije get(@PathVariable int id){
+        return emocijeRepository.findById(id).orElse(null);
+    }
+
+    @RequestMapping(path = "/emocije", method = RequestMethod.POST)
+    public Emocije create(@RequestBody Emocije emocije){
+        return emocijeRepository.save(emocije);
+    }
+
+    @RequestMapping(path = "/emocije", method = RequestMethod.PUT)
+    public Emocije update(@RequestBody Emocije emocije){
+        return emocijeRepository.save(emocije);
+    }
+
+    @RequestMapping(path = "/emocije", method = RequestMethod.DELETE)
+    public void delete(@PathVariable int id){
+        emocijeRepository.delete(emocijeRepository.findById(id).get());
+    }
+
 }
